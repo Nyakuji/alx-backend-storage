41c6a002ee8c129fff7e2a4dbe077be19c3088b6
philip Opiyo <opiyophilipo@gmail.com>
2024-05-10 20:51:49 +0300

    refactor: Update trigger to decrease quantity of an item after adding a new order

--- 0x00-MySQL_Advanced/5-valid_email.sql
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -8,10 +8,9 @@ CREATE TRIGGER valid_email
 BEFORE UPDATE ON users
 FOR EACH ROW
 BEGIN
-    IF OLD.email != NEW.email THEN
+    IF NEW.email <> OLD.email
+    THEN
         SET NEW.valid_email = 0;
-    ELSE
-        SET NEW.valid_email = 1;
     END IF;
 END $$
 DELIMITER ;
