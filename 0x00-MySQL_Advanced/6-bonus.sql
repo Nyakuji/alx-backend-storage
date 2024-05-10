6caebba3ba73b5ce9470120ce27c9ea1b55bebcc
philip Opiyo <opiyophilipo@gmail.com>
2024-05-10 20:06:14 +0300

    refactor: Update stored procedure for adding bonus corrections
    The SQL script `6-bonus.sql` has been modified to update the stored procedure `AddBonus` for adding a new correction for a student. The procedure now checks if the project exists in the `projects` table and inserts a new record if it doesn't. It also retrieves the `project_id` and inserts a new record into the `Corrections` table with the provided `user_id`, `project_id`, and `score`. This change improves the functionality and accuracy of the procedure.

--- 0x00-MySQL_Advanced/6-bonus.sql
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -1,19 +1,35 @@
--- creates a store procedure AddBonus 
--- that add a new correction for a student
-
+-- creates a stored procedure AddBonus 
+-- that adds a new correction for a student
+-- Drop the procedure if it already exists
 DROP PROCEDURE IF EXISTS AddBonus;
+
+-- Change the delimiter to handle the procedure definition
 DELIMITER $$
-CREATE PROCEDURE AddBonus(
-    IN user_id INT,
-    IN project_name VARCHAR(255),
-    IN score FLOAT
-)
+CREATE PROCEDURE AddBonus(user_id INT, project_name VARCHAR(255), score FLOAT)
 BEGIN
-    DECLARE project_id INT;
-    IF (SELECT COUNT(*) FROM projects WHERE name = project_name) = 0 THEN
-        INSERT INTO projects(name) VALUES(project_name);
+    DECLARE project_count INT DEFAULT 0;
+    DECLARE proj_id INT DEFAULT 0;
+
+    -- Check if the project already exists in the projects table
+    SELECT COUNT(id) INTO project_count
+    FROM projects
+    WHERE name = project_name
+    LIMIT 1; -- Limit the result to one row
+
+    -- If the project does not exist, insert it into the projects table
+    IF project_count = 0 THEN
+        INSERT INTO projects (name)
+        VALUES (project_name);
     END IF;
-    SET project_id = (SELECT id FROM projects WHERE name = project_name LIMIT 1);
-    INSERT INTO Corrections(user_id, project_id, score) VALUES(user_id, project_id, score); 
+
+    -- Retrieve the project_id for the specified project_name
+    SELECT id INTO proj_id
+    FROM projects
+    WHERE name = project_name
+    LIMIT 1; -- Limit the result to one row
+
+    -- Insert a new correction record into the corrections table
+    INSERT INTO corrections (user_id, project_id, score)
+    VALUES (user_id, proj_id, score);
 END $$
 DELIMITER ;
